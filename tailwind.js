d461e6786f07733ff453b49982337d490a3909d5
oliver-dvorski <oliver.dvorski@gmail.com>
2018-10-20 21:58:30 +0200

    Add more font weights and remove weights form the tailwind config that the serif font doesnt allow for

--- tailwind.js
+++ tailwind.js
@@ -293,15 +293,10 @@ module.exports = {
   */
 
   fontWeights: {
-    hairline: 100,
-    thin: 200,
-    light: 300,
     normal: 400,
     medium: 500,
     semibold: 600,
     bold: 700,
-    extrabold: 800,
-    black: 900
   },
 
   /*
