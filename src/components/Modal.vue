cc00d87e775582132fd094e4ad4054e919eaed1e
oliver-dvorski <oliver.dvorski@gmail.com>
2018-10-29 12:00:15 +0100

    Use esc to close modals

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -40,6 +40,13 @@ export default {
       type: String,
       default: ''
     }
+  },
+  mounted () {
+    document.addEventListener('keyup', (e) => {
+      if (this.show && e.key === 'Escape') {
+        this.$emit('close')
+      }
+    })
   }
 }
 </script>
