9e2f5348ae36ce95605e75306d4a7207637176f4
Oliver <oliver.dvorski@gmail.com>
2020-12-24 18:31:05 +0000

    Add dist files

---
+++ dist/precache-manifest.29723d327b3698df8b4fdc1514232a3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e4653c1854f790ef0626",
+    "url": "/js/chunk-vendors.3fae9a28.js"
+  },
+  {
+    "revision": "e865e9c4877e6f488136",
+    "url": "/js/app.27827468.js"
+  },
+  {
+    "revision": "ede3863c450abd7851af4c36951dbf50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e865e9c4877e6f488136",
+    "url": "/css/app.8ea6e205.css"
+  }
+];
